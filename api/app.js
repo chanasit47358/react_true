96128e5a565614e14ef55f81ffd9ee5a486f74c4
tessuka30 <83247616+tessuka30@users.noreply.github.com>
2021-05-19 05:34:43 +0700

    2

--- api/app.js
+++ api/app.js
@@ -14,12 +14,12 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "ExampleApi",
+      title: "Api",
       version: "1.0.0",
     },
     servers: [
       {
-        url: "http://localhost:8080",
+        url: "http://localhost:4000",
       },
     ],
   },
