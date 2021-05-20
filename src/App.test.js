c0f82138e4c2ed3415d90fe43dc00aed089822ae
tessuka30 <tessuka30@gmail.com>
2021-05-20 14:33:23 +0700

    2021-05-20 14:33

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import App from './interpolation/Sp.js';
-
-test('Cubic Spline', () => {
+import App from "./interpolation/Sp.js";
+import App1 from "./linear/jacobi/Jacobi.js";
+import App2 from "./linear/cramer/Cramer.js";
+import App3 from "./linear/conjugate/Conjugate.js";
+import App4 from "./linear/gauss_eli/Gausseli.js";
+test("Cubic Spline", () => {
   render(<App />);
   expect(screen.getByText("Cubic Spline")).toBeInTheDocument();
+});
+test("Jacobi", () => {
+  render(<App1 />);
+  expect(screen.getByText("Jacobi")).toBeInTheDocument();
+});
+test("Cramer", () => {
+  render(<App2 />);
+  expect(screen.getByText("Cramer")).toBeInTheDocument();
+});
+test("Gauss Elimination", () => {
+  render(<App4 />);
+  expect(screen.getByText("Gauss Elimination")).toBeInTheDocument();
+});
+test("Conjugate Gradient", () => {
+  render(<App3 />);
+  expect(screen.getByText("Conjugate Gradient")).toBeInTheDocument();
 });
